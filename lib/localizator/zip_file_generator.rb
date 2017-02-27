3ad0ac5f7d0f7e4190f68527056ab26f2da1f34b
rafael <skcc321@gmail.com>
2017-02-27 23:22:40 +0200

    controller tests

--- lib/localizator/zip_file_generator.rb
+++ lib/localizator/zip_file_generator.rb
@@ -1,16 +1,6 @@
 require 'zip'
 
 module Localizator
-  # This is a simple example which uses rubyzip to
-  # recursively generate a zip file from the contents of
-  # a specified directory. The directory itself is not
-  # included in the archive, rather just its contents.
-  #
-  # Usage:
-  #   directory_to_zip = "/tmp/input"
-  #   output_file = "/tmp/out.zip"
-  #   zf = ZipFileGenerator.new(directory_to_zip, output_file)
-  #   zf.write()
   class ZipFileGenerator
     # Initialize with the directory to zip and the location of the output archive.
     def initialize(input_dir, output_file)
@@ -34,7 +24,6 @@ module Localizator
       entries.each do |e|
         zip_file_path = path == '' ? e : File.join(path, e)
         disk_file_path = File.join(@input_dir, zip_file_path)
-        puts "Deflating #{disk_file_path}"
 
         if File.directory? disk_file_path
           recursively_deflate_directory(disk_file_path, io, zip_file_path)
--- test/controllers/localizator/locales_controller_test.rb
+++ test/controllers/localizator/locales_controller_test.rb
@@ -12,7 +12,14 @@ module Localizator
     end
 
     test "should post update" do
+      locales_file = Rails.root.join('config', 'locales', 'en.yml')
+
+      assert_equal(YAML.load_file(locales_file)['en']['hello'], 'hello')
       post :update, translations: { "en.hello" => 'Hello', "uk.hello" => "Привіт", "ru.hello" => 'Привет' }
+      assert_equal(YAML.load_file(locales_file)['en']['hello'], 'Hello')
+      post :update, translations: { "en.hello" => 'hello', "uk.hello" => "привіт", "ru.hello" => 'привет' }
+      assert_equal(YAML.load_file(locales_file)['en']['hello'], 'hello')
+
       assert_redirected_to locales_path
     end
 
@@ -22,8 +29,10 @@ module Localizator
     end
 
     test "should get reload" do
+      File.delete(Rails.root.join('tmp', 'restart.txt'))
       get :reload
       assert_redirected_to locales_path
+      assert_equal(File.exist?(Rails.root.join('tmp', 'restart.txt')), true)
     end
   end
 end
